167200447c2abe25bc72da4323da726e61bb28fc
jsn 筆電 <fdjkgh580@yahoo.com.tw>
2015-10-07 11:50:25 +0800

    文件修正

--- doc/index.js
+++ doc/index.js
@@ -1,95 +1,106 @@
     $(function (){
-        $(window).vmodel("--scroll", true, function (){
-            var vs = this;
-            this.autoload = ['bind'];
-            this.bind = function (){
-                vs.root.scroll(function (){
-                    var mtop = vs.root.scrollTop();
-                    var header = $.vmodel.get("header")
-                    if (mtop > 150) {
-                        header.min();
-                    } 
-                    else {
-                        header.max();
-                    }
-                })
-            }
-        });
+        // $(window).vmodel("--scroll", true, function (){
+        //     var vs = this;
+        //     this.autoload = ['bind'];
+        //     this.bind = function (){
+        //         vs.root.scroll(function (){
+        //             var mtop = vs.root.scrollTop();
+        //             var header = $.vmodel.get("header")
+        //             if (mtop > 150) {
+        //                 header.min();
+        //             } 
+        //             else {
+        //                 header.max();
+        //             }
+        //         })
+        //     }
+        // });
 
 
-        $("header").vmodel("--header", true, function (){
-            var vs = this;
+        // $("header").vmodel("--header", true, function (){
+        //     var vs = this;
 
 
-            this.autoload = []
+        //     this.autoload = []
 
 
-            this.min = function (){
-                vs.hide_bar();
-            }
+        //     this.min = function (){
+        //         vs.hide_bar();
+        //     }
 
-            this.max = function (){
-                vs.show_bar();
-            }
+        //     this.max = function (){
+        //         vs.show_bar();
+        //     }
 
-            this.min_logo = function (){
-                vs.root.find(".logo").css({
-                    position: 'fixed',
-                    left: 0,
-                    right:0,
-                    top:0,
-                    fontSize: "20px",
-                    zIndex: 10
-                })
-            }
-            this.max_logo = function (){
-                vs.root.find(".logo").removeAttr('style');
-            }
+        //     this.min_logo = function (){
+        //         vs.root.find(".logo").css({
+        //             position: 'fixed',
+        //             left: 0,
+        //             right:0,
+        //             top:0,
+        //             fontSize: "20px",
+        //             zIndex: 10
+        //         })
+        //     }
+        //     this.max_logo = function (){
+        //         vs.root.find(".logo").removeAttr('style');
+        //     }
 
-            this.lock_set = function (bool){
-                vs.root.find(".logobar").attr("data-lock", bool);
-            }
+        //     this.lock_set = function (bool){
+        //         vs.root.find(".logobar").attr("data-lock", bool);
+        //     }
 
-            this.hide_bar = function (){
-                if (vs.root.find(".logobar").attr("data-lock") == "true") return false;
-                if (vs.root.hasClass('min')) return false;
-                vs.lock_set(true);
+        //     this.hide_bar = function (){
+        //         if (vs.root.find(".logobar").attr("data-lock") == "true") return false;
+        //         if (vs.root.hasClass('min')) return false;
+        //         vs.lock_set(true);
 
-                vs.root.find(".logobar").animate({
-                    'opacity': 0
-                }, 0, function (){
-                    vs.root.addClass("min");
-                    vs.min_logo();
-                    vs.lock_set(false);
-                })
-            }
-            this.show_bar = function (){
-                if (!vs.root.hasClass('min')) return false;
-                vs.max_logo();
+        //         vs.root.find(".logobar").animate({
+        //             'opacity': 0
+        //         }, 0, function (){
+        //             vs.root.addClass("min");
+        //             vs.min_logo();
+        //             vs.lock_set(false);
+        //         })
+        //     }
+        //     this.show_bar = function (){
+        //         if (!vs.root.hasClass('min')) return false;
+        //         vs.max_logo();
 
-                var h = vs.default_height;
-                vs.root.find(".logobar").animate({
-                    'opacity': 1
-                }, function (){
-                    vs.root.removeClass("min");
-                })
-            }
-        });
+        //         var h = vs.default_height;
+        //         vs.root.find(".logobar").animate({
+        //             'opacity': 1
+        //         }, function (){
+        //             vs.root.removeClass("min");
+        //         })
+        //     }
+        // });
         
         // 建立目錄
         $(".dir").vmodel("--dir", false, function (){
             var vs = this;
-            this.autoload = ['create'];
+            this.autoload = ['create', 'goto'];
             this.create = function (){
                 var ary = $.vmodel.get("doctitle").get();
                 vs.set(ary);
             }
+            //建立目錄
             this.set = function (ary){
                 var html = '';
                 $.each(ary, function (key, text){
-                    html += '<li>' + text + '</li>';
+                    html += '<li><a class="goto" data-goto="' + key + '" href="">' + text + '</a></li>';
                 });
                 vs.root.html(html);
+            }
+            
+            // 綁定前往
+            this.goto = function (){
+                vs.root.on("click", ".goto", function (){
+                    var eq = $(this).attr("data-goto");
+                    $.vmodel.get("doctitle").go(eq);
+                    return false;
+                });
+                
             }
         });
 
@@ -103,6 +114,14 @@
                     mix[key] = $(ele).text().trim();
                 })
                 return mix;
+            }
+            // 前往
+            this.go = function (eq){
+                var px = vs.root.find("h2").eq(eq).offset().top;
+                $("body").animate({
+                    scrollTop : px
+                });
+                console.log(top);
             }
         });
         $.vmodel.get("dir", true);
