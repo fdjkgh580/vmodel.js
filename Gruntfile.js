106ff24bcee4418be715bf8795b324977074b4d9
jsn 筆電 <fdjkgh580@yahoo.com.tw>
2015-11-06 12:17:38 +0800

    自動產出完成

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,37 +7,28 @@ module.exports = function(grunt) {
         uglify: {
             options: {
                 // 設定壓縮後檔頭要插入的註解
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */',
                 // 使用 SourceMap 並且將 JS Source 與 Map 檔案放在一起
                 sourceMap: true,
                 sourceMapIncludeSources: true
             },
             my_target: {
-                files: [{
-                    // 啟用擴展
-                    expand: true,
-                    // 來源的路徑
-                    cwd: 'plugin/JS',
-                    // 將不是 .min.js 的檔案全部進行壓縮
-                    src: ['**/*.js', '!*.min.js'],
-                    // 輸出的路徑
-                    dest: 'plugin/JS',
-                    // 額外壓縮成 .min.js 而不會覆蓋
-                    ext: '.min.js'
-                }]
+                files: {
+                    'src/jquery.vmodel.min.js': ['src/jquery.vmodel.js']
+                }
             }
         },
         // 設定監控異動
         watch: {
             //監控的檔案
-            files: ['**/*'],
+            files: ['src/*.js'],
             // 監控發生異動，任務要指派給哪個外掛套件
-            tasks: ['concat', 'uglify', 'concat'],
+            tasks: ['uglify'],
             options: {
                 // 間隔多久執行監控
                 interval: 100,
                 // 自動重整
-                livereload: true
+                livereload: false
             }
         },
     });
